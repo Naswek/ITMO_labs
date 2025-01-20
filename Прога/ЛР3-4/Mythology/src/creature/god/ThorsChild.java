581a0c5423b7d50ecdc7ef08117718f4a260ecb3
Naswek <161589046+Naswek@users.noreply.github.com>
2025-01-20 14:40:40 +0300

    Update ThorsChild.java

--- Прога/ЛР3-4/Mythology/src/creature/god/ThorsChild.java
+++ Прога/ЛР3-4/Mythology/src/creature/god/ThorsChild.java
@@ -1,10 +1,10 @@
 package creature.god;
 
 public record ThorsChild() {
-    protected static String childName;
-    protected static int childAge;
-    protected static String weapon;
-    protected static String mother;
-    protected static double strength;
+    private static String childName;
+    private static int childAge;
+    private static String weapon;
+    private static String mother;
+    private static double strength;
 
 }
