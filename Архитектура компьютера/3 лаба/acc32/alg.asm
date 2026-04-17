d24245e204510d1ba47a42beee19e32342b1ca31
Naswek <nastya.rovkova06@gmail.com>
2026-04-17 18:34:29 +0300

    add wrench link for acc32

--- Архитектура компьютера/3 лаба/acc32/alg.asm
+++ Архитектура компьютера/3 лаба/acc32/alg.asm
@@ -2,13 +2,13 @@
 
 input_addr:     .word  0x80
 output_addr:    .word  0x84
-a_word:         .word 0             ;a   
+a_word:         .word 0             ;a
 b_word:         .word 0             ;b
 curr: 		    .word 0
-mask: 		    .word 0x7FFFFFFF    
+mask: 		    .word 0x7FFFFFFF
 error_b:	    .word -1
 error_carry:	.word 0xCCCCCCCC
-		
+
 
   .text
   .org 0x100
@@ -19,16 +19,16 @@ load_arg:
     ble             if_input_is_0   ;check if a != 0
 	store	 	    a_word
 	load 			input_addr      ;load b
-    load_acc       
+    load_acc
     ble             if_input_is_0   ;check if b != 0
 	store		    b_word
 
 loop:
-	load_addr 	a_word 
+	load_addr 	a_word
 	rem 	  	b_word              ;a % b
 	store_addr 	curr                ;store a % b
 
-	load_addr 	b_word      
+	load_addr 	b_word
 	store_addr 	a_word              ;a = b
 
 	load_addr 	curr
@@ -40,9 +40,9 @@ loop:
 	jmp 		loop                ;b != 0 -> loop
 
 done:
-	load_addr 	a_word	
+	load_addr 	a_word
 	and 		mask                ;clear 15 bit
-    jmp write_and_stop              
+    jmp write_and_stop
 
 if_input_is_0:
 	load_addr	error_b             ;load value if a or b == 0
@@ -56,3 +56,5 @@ write_and_stop:
     store_ind 	output_addr         ;store output
 	halt                            ;stop machine
 
+
+https://wrench.edu.swampbuds.me/report/256dbc80-fb72-479f-b8c4-11b4196a6ba2
